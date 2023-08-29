ed67307591047d2b58a070296aad05e99ef97edd
BryceKnight16 <6373@holbertonstudents.com>
2023-08-28 20:30:07 -0700

    a function that takes a float and returns a str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module turns a float into a str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    fucntion that takes a float and returns a str
+    """
+    return str(n)
