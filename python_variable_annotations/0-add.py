ba956d7bb129f2cdf7b8695390da4feef0568648
BryceKnight16 <6373@holbertonstudents.com>
2023-08-28 20:13:13 -0700

    function that adds to values and one that concats two strings

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module has the function add
+"""
+
+def add (a: float, b: float) -> float:
+    """
+    adds to variables together
+    """
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that concats two strings
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    Concats to two string
+    """
+    return a + b
