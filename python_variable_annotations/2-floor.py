25600f6f2e617e92ff1a55f518ef68d42fe0a465
BryceKnight16 <6373@holbertonstudents.com>
2023-08-28 20:26:09 -0700

    a function that finds the floor of the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that finds the floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function that finds the floor of the float
+    """
+    return math.floor(n)
