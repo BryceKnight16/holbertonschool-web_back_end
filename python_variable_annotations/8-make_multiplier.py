f04f877b743785456faf2a363988badb9693b953
BryceKnight16 <6373@holbertonstudents.com>
2023-08-29 20:25:09 -0700

    using callable to add the return of one function to another function

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,7 @@ and multpile the variable mutliplier to the do multiple function
 from typing import Callable
 
 
-def make_multiplier(mutliplier: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     function that takes a float multipler as an arg and returns
     a function that multiplies a float by the multiplier
