17a43f68067293d2e156de4bfc802227e4e32859
BryceKnight16 <6373@holbertonstudents.com>
2023-08-29 20:01:12 -0700

    function that uses union to take str and int/float as args and returns a tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that uses union to take a str and in/float and returns a tuple
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float) -> Tuple[str, float]:
+          """
+          function that takes a int/float and str and turns it into a tuple
+          """
+          return (k, v**2)
