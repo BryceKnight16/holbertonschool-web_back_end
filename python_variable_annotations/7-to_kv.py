2d7afee181752c974b4b29af8994927e485e9363
BryceKnight16 <6373@holbertonstudents.com>
2023-08-29 20:04:12 -0700

    changing bracket to be correct one

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ Module that uses union to take a str and in/float and returns a tuple
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
           """
           function that takes a int/float and str and turns it into a tuple
           """
