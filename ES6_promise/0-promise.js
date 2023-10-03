50209615262a32b18969168a95b3e0b2ba7ff34a
BryceKnight16 <6373@holbertonstudents.com>
2023-10-03 11:56:37 +1100

    eslint still annyoing me

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve) => {
-   setTimeout(() => {
-   resolve('true');
+    setTimeout(() => {
+      resolve('true');
     }, 2000);
   });
 }
